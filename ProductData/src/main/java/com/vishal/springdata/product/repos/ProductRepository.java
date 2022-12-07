f56e1efc875704f025e71e3413f532f5a75aa3d9
vishal <vishalambekar459@gmail.com>
2022-12-07 14:13:33 +0530

    paging and sorting added

--- ProductData/src/main/java/com/vishal/springdata/product/repos/ProductRepository.java
+++ ProductData/src/main/java/com/vishal/springdata/product/repos/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.vishal.springdata.product.repos;
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -17,5 +18,9 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 	List<Product> findByPriceBetween(Double price1, Double price2);
 	List<Product> findByDescLike(String desc);
 	List<Product> findByIdIn(List<Integer> ids);
+	
+	//custom finder method for paging
+	List<Product> findByIdIn(List<Integer> ids,Pageable pageable);
+	
 	
 }
--- ProductData/src/test/java/com/vishal/springdata/product/PagingAndSortingTest.java
+++ ProductData/src/test/java/com/vishal/springdata/product/PagingAndSortingTest.java
@@ -22,11 +22,13 @@ public class PagingAndSortingTest {
 	
 	@Test
 	public void testFindAllPaging() {
-		Pageable pageable = PageRequest.of(1, 2);    //1 is page number and 2 is page size
+		Pageable pageable = PageRequest.of(0, 2);    //1 is page number and 2 is page size...page starts with 0
 		Page<Product> results = repo.findAll(pageable);
-		
 		results.forEach(p -> System.out.println("Paging --- Product name :: "+p.getName()));
 	}
+	
+	
+	//---------------------------------------------sorting----------------------------------------------------------------------------------
 	
 	@Test
 	public void testFindAllSortingByName() {
@@ -34,9 +36,54 @@ public class PagingAndSortingTest {
 		System.out.println("results sorting by name:: "+results);
 	}
 	
+	
 	@Test
 	public void testFindAllSortingByDescription() {
 		List<Product> results = repo.findAll(Sort.by("desc"));
 		System.out.println("results sorting by Desc:: "+results);
 	}
+	
+	
+	@Test
+	public void testFindAllSortingByNameAndDescription() {
+		List<Product> results = repo.findAll(Sort.by("desc","name"));
+		System.out.println("results sorting by name and desc:: "+results);
+	}
+	
+	@Test
+	public void testFindAllSortingByPriceAndId() {
+		List<Product> results = repo.findAll(Sort.by("price","id"));
+		System.out.println("results sorting by price and id :: "+results);
+	}
+	
+	
+	//---------------------------------------------Paging and sorting--------------------------------------------------------
+	
+	@Test
+	public void testFindAllPagingAndSorting() {
+		Pageable pageable = PageRequest.of(0, 2, Direction.DESC,"id");
+		repo.findAll(pageable).forEach(p -> System.out.println("Paging and sorting test with id ::"+p));
+	}
+	
+	@Test
+	public void testFindAllPagingAndSortingIdAndName() {
+		Pageable pageable = PageRequest.of(0, 2, Direction.DESC,"id","name");
+		repo.findAll(pageable).forEach(p -> System.out.println("Paging and sorting test with id and name ::"+p));
+	}
+	
+	
+	//Custom finder method in repository
+	
+	@Test
+	public void testFindByIdsInWithPaging() {
+		List<Product> products = repo.findByIdIn(List.of(1,2,3),PageRequest.of(0, 2));
+		System.out.println("products find Ids in with paging :: "+products);
+	}
+	
+	@Test
+	public void testFindByIdsInWithPagingAndSorting() {
+		List<Product> products = repo.findByIdIn(List.of(1,2,3),PageRequest.of(0, 2,Direction.DESC,"id"));
+		System.out.println("products find Ids in with paging and sorting :: "+products);
+	}
+	
 }
