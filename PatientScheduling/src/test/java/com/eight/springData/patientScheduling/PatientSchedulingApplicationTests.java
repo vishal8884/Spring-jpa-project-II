41c8039f73fc88680681970ccdb96df76f5f037c
vishal <vishalambekar459@gmail.com>
2023-01-04 23:40:11 +0530

    entities and repos created

---
+++ PatientScheduling/src/test/java/com/eight/springData/patientScheduling/PatientSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eight.springData.patientScheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
