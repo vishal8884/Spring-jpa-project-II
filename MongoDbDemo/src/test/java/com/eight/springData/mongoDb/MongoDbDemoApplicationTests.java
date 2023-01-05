d24effa933474149b6ed665d250169437bdf3097
vishal <vishalambekar459@gmail.com>
2023-01-05 14:44:01 +0530

    mongo db tests added

--- MongoDbDemo/src/test/java/com/eight/springData/mongoDb/MongoDbDemoApplicationTests.java
+++ MongoDbDemo/src/test/java/com/eight/springData/mongoDb/MongoDbDemoApplicationTests.java
@@ -2,6 +2,8 @@ package com.eight.springData.mongoDb;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,6 +29,20 @@ class MongoDbDemoApplicationTests {
 		System.out.println("savedProduct :: "+savedProduct);
 		
 		assertNotNull(savedProduct);
+	}
+	
+	@Test
+	public void testFindAll() {
+		List<Product> products = repo.findAll();
+		System.out.println("products :: "+products);
+	}
+	
+	@Test
+	public void testUpdate() {
+		Product product = repo.findById("63b68e3feecb5e3921257544").get();
+		
+		product.setName("i phone");
+		repo.save(product);
 	}
 
 }
